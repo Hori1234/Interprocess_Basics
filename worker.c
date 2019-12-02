3ab2e7c946a141d9e90b598aefe6a591b07e2be2
Hori1234 <31578610+Hori1234@users.noreply.github.com>
2019-12-02 20:58:56 +0100

    Add files via upload

--- worker.c
+++ worker.c
@@ -26,20 +26,79 @@
 #include "md5s.h"
 
 static void rsleep (int t);
-
+static bool findHash(char Fc, char Lc, char word[], uint128_t encryptedWord, char* solution, int messageLength, int k);
+static bool compare(uint128_t check[],uint128_t result[]);
 
 int main (int argc, char * argv[])
 {
     // TODO:
     // (see message_queue_test() in interprocess_basic.c)
-    //  * open the two message queues (whose names are provided in the arguments)
+    //  * open the two message queues (whose names
+    mqd_t m_com_request;
+    mqd_t m_com_response;
+
+    MQ_REQUEST_MESSAGE req;
+    MQ_RESPONSE_MESSAGE res; 
+    
+    m_com_request = mq_open(mq_name1,O_RDONLY);
+    m_com_response = mq_open(mq_name2,O_WRONLY);
+    //      are provided in the arguments)
+    bool havingJobs = true;
     //  * repeatingly:
+    while(havingJobs){
     //      - read from a message queue the new job to do
+            mq_receive(m_com_request,(char*) &req, sizeof(req),NULL);
+
     //      - wait a random amount of time (e.g. rsleep(10000);)
+            rsleep(10000);
+            
+            if (req.length = 0){
+                havingJobs = false;
+            } else {
+            
+            //define a temp variable to store the first char
+            
+            char word[1];
+            word[0] = req.Fc;
+
+                //initialize the sollution array
+            char sollution[req.length];
+    
     //      - do that job 
+              bool hashFound = findHash(req.Fc,req.Lc,word,req.md5,sollution,req.length,1);  
+
+              if(hashFound){
+
+                // Copying the solution in the new message
+                for (int i=0; i< sizeof(req); i++){
+                    res.message[i] = sollution[i];
+                }
+
+                // Filling the rest of the message in order to reach the 
+                // final length
+                for (int i = req.length; i < sizeof(res.message); i++)
+                {
+                    res.message[i] = (char) 0;
+                }
+
+                res.length = strlen(sollution);
+              } else {
+                
+                res.length = 0;
+              
+              }
     //      - write the results to a message queue
+              mq_send(m_com_response,(char *) &res,sizeof(res),0);
+
+            }
+    
+            
     //    until there are no more tasks to do
+    }
+    
     //  * close the message queues
+    mq_close(m_com_request);
+    mq_close(m_com_response);
     
     return (0);
 }
@@ -63,4 +122,45 @@ static void rsleep (int t)
     usleep (random() % t);
 }
 
+static bool hashFound(char Fc, char Lc, char word[], uint128_t encryptedWord, char* solution, int messageLength, int k){
 
+    uint128_t hash;
+    hash = md5s(word,strlen(word));
+
+    bool hasIsFound = false;
+
+    if(strlen(hash) == strlen(encryptedWord)){
+
+        if (compare(hash,encryptedWord)) {
+            strcpy(solution,word);
+            hasIsFound = true;
+        } 
+    } else {
+
+        if (strlen(word) < messageLength && hasIsFound == false){
+
+            for (char c = Fc; c <= Lc; c++){
+
+                  word[k] = c;
+                  if (hashFound(Fc,Lc,word,encryptedWord,solution,messageLength,k+1)){
+                    hasIsFound = true;
+                  }          
+
+            }
+        }
+    }
+
+    return hashFound;
+}
+
+// Comparing the hashed string with the original one
+static bool compare(uint128_t check[], uint128_t result[]){
+    
+    for (int i=0; i < strlen(result); i++){
+        
+        if (check[i] != result[i]){
+            return false;
+        }
+    }
+    return true;
+}
